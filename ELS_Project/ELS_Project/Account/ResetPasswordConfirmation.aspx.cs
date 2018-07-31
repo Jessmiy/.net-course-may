d43a666266f7fc02e8ce62b9114f055acb811c70
Jess <jessmiy20@gmail.com>
2018-07-31 13:52:59 +0800

    initial commit

---
+++ ELS_Project/ELS_Project/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ELS_Project.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ELS_Project/ELS_Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ELS_Project.Global" Language="C#" %>
